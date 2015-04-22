816b3d0404b944034be892e6c4ed4d2db3e17dc3
Gagan Bansal <gaganbansal123@gmail.com>
2015-04-22 03:31:52 -0400

    added support for FeatureCollection and GeometryCollection

--- test/test.js
+++ test/test.js
@@ -1,79 +1,79 @@
 var expect = require('chai').expect,
   reproject = require('../'),
   GeoEq = require('geojson-equality'),
-  set = require('./test-data.js');
+  testData = require('./test-data.js');
 
 describe('reproject spherical mercator', function() {
   var eq = new GeoEq({precision: 5});
   it('Point', function() {
     expect(
       eq.compare(
-        reproject(set['Point'].input),
-        set['Point'].output
+        reproject(testData['Point'].input),
+        testData['Point'].output
       )
     ).to.be.true;
   });
   it('LineString', function() {
     expect(
       eq.compare(
-        reproject(set['LineString'].input),
-        set['LineString'].output
+        reproject(testData['LineString'].input),
+        testData['LineString'].output
       )
     ).to.be.true;
   });
   it('Polygon', function() {
     expect(
       eq.compare(
-        reproject(set['Polygon'].input),
-        set['Polygon'].output
+        reproject(testData['Polygon'].input),
+        testData['Polygon'].output
       )
     ).to.be.true;
   });
   it('Polygon with hole', function() {
     expect(
       eq.compare(
-        reproject(set['Polygon with hole'].input),
-        set['Polygon with hole'].output
+        reproject(testData['Polygon with hole'].input),
+        testData['Polygon with hole'].output
       )
     ).to.be.true;
   });
   it('MultiPoint', function() {
     expect(
       eq.compare(
-        reproject(set['MultiPoint'].input),
-        set['MultiPoint'].output
+        reproject(testData['MultiPoint'].input),
+        testData['MultiPoint'].output
       )
     ).to.be.true;
   });
   it('MultiLineString', function() {
     expect(
       eq.compare(
-        reproject(set['MultiLineString'].input),
-        set['MultiLineString'].output
+        reproject(testData['MultiLineString'].input),
+        testData['MultiLineString'].output
       )
     ).to.be.true;
   });
   it('MultiPolygon', function() {
     expect(
       eq.compare(
-        reproject(set['MultiPolygon'].input),
-        set['MultiPolygon'].output
+        reproject(testData['MultiPolygon'].input),
+        testData['MultiPolygon'].output
       )
     ).to.be.true;
   });
   it('MultiPolygon with hole', function() {
     expect(
       eq.compare(
-        reproject(set['MultiPolygon with hole'].input),
-        set['MultiPolygon with hole'].output
+        reproject(testData['MultiPolygon with hole'].input),
+        testData['MultiPolygon with hole'].output
       )
     ).to.be.true;
   });
   it('Feature', function() {
     expect(
       eq.compare(
-        reproject(set['Feature'].input),
-        set['Feature'].output
+        reproject(testData['Feature'].input),
+        testData['Feature'].output
       )
     ).to.be.true;
   });
@@ -81,5 +81,27 @@ describe('reproject spherical mercator', function() {
     expect(
       reproject.bind(this,{type:"Geometry", coordinates: [10,20]})
     ).to.throw(Error);
+  });
+  it('FeatureCollection', function() {
+    var projected = reproject(testData['FeatureCollection'].input);
+    expect(projected).to.have.property('type','FeatureCollection');
+    expect(
+      testData['FeatureCollection'].output.features.every(function(expFeat) {
+        return projected.features.some(function(projFeat) {
+          return eq.compare(projFeat,expFeat);
+        });
+      })
+    ).to.be.true;
+  });
+  it('GeometryCollection', function() {
+    var projected = reproject(testData['GeometryCollection'].input);
+    expect(projected).to.have.property('type','GeometryCollection');
+    expect(
+      testData['GeometryCollection'].output.geometries.every(function(expFeat) {
+        return projected.geometries.some(function(projFeat) {
+          return eq.compare(projFeat,expFeat);
+        });
+      })
+    ).to.be.true;
   });
 });
